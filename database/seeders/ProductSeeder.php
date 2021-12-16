174977d1e8eb3dfc8c2667c9357214944f552255
= <=>
2021-12-16 14:32:17 +0100

    minor price changes

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -22,21 +22,21 @@ class ProductSeeder extends Seeder
                 'name' => 'Beauty Treatment', 
                 'image' => '/images/beauty.jpg', 
                 'description' => 'The best beauty products in town. Get the best price.',
-                'price' => 10000, 
+                'price' => 150000, 
                 'in_stock' => 30
             ],
             [
                 'name' => 'Camera', 
                 'image' => '/images/camera.jpg', 
                 'description' => 'The latest in image technology. Buy for this one-time low price',
-                'price' => 40000, 
+                'price' => 120000, 
                 'in_stock' => 50
             ],
             [
                 'name' => 'Men\'s Shoes', 
                 'image' => '/images/shoes.jpg', 
                 'description' => 'Clasic men\'s italian leather shoes. Get the best price anywhere in the market',
-                'price' => 30000, 
+                'price' => 80000, 
                 'in_stock' => 10
             ]
         ));
